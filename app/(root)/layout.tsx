dedecfcca7cfabd056a0f04b740ca3174c9d5ce3
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-14 11:21:55 -0600

    feat(Rightsidebar): implement Rightsidebar component initial layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <Topbar />
 
-          <main>
+          <main className="flex flex-row">
             <Leftsidebar />
 
             <section className="main-container">
