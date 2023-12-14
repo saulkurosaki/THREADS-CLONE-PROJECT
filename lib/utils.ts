2c546afa561f3246f4cb9d931e741fea41a7d962
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-14 16:59:16 -0600

    feat(utils.ts): implement format-thread-count utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -27,3 +27,13 @@ export function formatDateString(dateString: string) {
 
   return `${time} - ${formattedDate}`;
 }
+
+export function formatThreadCount(count: number): string {
+  if (count === 0) {
+    return "No Threads";
+  } else {
+    const threadCount = count.toString().padStart(2, "0");
+    const threadWord = count === 1 ? "Thread" : "Threads";
+    return `${threadCount} ${threadWord}`;
+  }
+}
