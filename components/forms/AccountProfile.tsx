097194391c8923d10894991510600bc4c1e82975
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-14 12:25:25 -0600

    feat(AccountProfile): implement name form field

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -95,6 +95,25 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             </FormItem>
           )}
         />
+
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Name
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <Input
+                  type="text"
+                  className="account-form_input no-focus"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
