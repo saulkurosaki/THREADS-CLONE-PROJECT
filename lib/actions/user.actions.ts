4464e66ae9b03fa80d33c1b2cef913c474b57fda
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-14 19:39:56 -0600

    feat(user.actions.ts): implement update-user server action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,13 +1,36 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
-export const updateUser = async (): Promise<void> => {
+export const updateUser = async (
+  userId: string,
+  username: string,
+  name: string,
+  bio: string,
+  image: string,
+  path: string
+): Promise<void> => {
   connectToDB();
 
   try {
-  } catch (error) {
-    console.log(error);
-    throw error;
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded: true,
+      },
+      { upsert: true }
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
   }
 };
