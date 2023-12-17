421756ba62e233aadae84f34027e1ee5400ab677
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-16 19:12:41 -0600

    feat(thread.actions.ts): implement fetchPosts server action

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import { skip } from "node:test";
 
 interface Params {
   text: string;
@@ -35,5 +36,41 @@ export const createThread = async ({
     revalidatePath(path);
   } catch (error: any) {
     throw new Error(`Error creating thread: ${error.message}`);
+  }
+};
+
+export const fetchPosts = async (pageNumber = 1, pageSize = 20) => {
+  connectToDB();
+
+  try {
+    // Calculate the number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Fetch the posts that have no parents (top-level threads...)
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalPostsCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+  } catch (error) {
+    throw new Error(`Error getting the posts: ${error}`);
   }
 };
