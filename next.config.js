82124dcf36c0f4b6c5e2e06ee7d79101a71630c1
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-17 11:10:19 -0600

    fix(next.config): add a new image provider domain name to the accepted ones

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "placehold.co",
       },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
     ],
   },
 };
