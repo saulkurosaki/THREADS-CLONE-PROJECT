caa60641dac0ee3da13834bc97a9f24a09470f57
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-16 18:36:13 -0600

    feat(thread.actions.ts): implement createThread server action

---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,35 @@
+import { revalidatePath } from "next/cache";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+
+interface Params {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+}
+
+export const createThread = async ({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) => {
+  connectToDB();
+
+  try {
+    const createThread = await Thread.create({
+      text,
+      author,
+      community: null,
+    });
+
+    // Update user model
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createThread._id },
+    });
+
+    revalidatePath(path);
+  } catch (error) {}
+};
