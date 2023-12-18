8fe23a2277c2a64e5cd8d7c376e3c6edfc6da7a7
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-18 17:37:17 -0600

    implement activity page initial layout

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ActivityPage = async () => {
+  return (
+    <section>
+      <h1 className="head-text mb-10">Activity</h1>
+    </section>
+  );
+};
+
+export default ActivityPage;
