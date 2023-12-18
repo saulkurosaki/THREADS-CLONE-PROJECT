4a45812f2df3bb4fa5fb2231086d8d3a434239be
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-18 16:44:36 -0600

    feat(ThreadTab): implement ThreadTab component initial layout

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,6 +1,10 @@
 import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 const Profile = async ({ params }: { params: { id: string } }) => {
@@ -20,6 +24,44 @@ const Profile = async ({ params }: { params: { id: string } }) => {
         imgUrl={userInfo.image}
         bio={userInfo.bio}
       />
+
+      <div className="mt-9">
+        <Tabs defaultValue="threads" className="w-full">
+          <TabsList className="tab">
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className="tab">
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className="object-contain"
+                />
+                <p className="max-sm:hidden">{tab.label}</p>
+
+                {tab.label === "Threads" && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo?.threads?.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          {profileTabs.map((tab) => (
+            <TabsContent
+              key={`content-${tab.label}`}
+              value={tab.value}
+              className="w-full text-light-1"
+            >
+              <ThreadsTab
+                currentUser={user.id}
+                accountId={userInfo.id}
+                accountType="User"
+              />
+            </TabsContent>
+          ))}
+        </Tabs>
+      </div>
     </div>
   );
 };
---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+  currentUser: string;
+  accountId: string;
+  accountType: string;
+}
+
+const ThreadsTab = ({ currentUser, accountId, accountType }: Props) => {
+  return <div>Threads Tab Test By Saul c:</div>;
+};
+
+export default ThreadsTab;
