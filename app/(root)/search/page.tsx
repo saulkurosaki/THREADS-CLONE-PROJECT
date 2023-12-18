8405e4468f7c7f032726f59cbe10bd84cb12808f
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-18 17:38:27 -0600

    implement search page initial layout

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchPage = async () => {
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+    </section>
+  );
+};
+
+export default SearchPage;
