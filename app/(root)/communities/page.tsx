aef4993b0193cb24731595745aac934c74bfea83
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-18 17:37:43 -0600

    implement communities page initial layout

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CommunitiesPage = async () => {
+  return (
+    <section>
+      <h1 className="head-text mb-10">Communities</h1>
+    </section>
+  );
+};
+
+export default CommunitiesPage;
