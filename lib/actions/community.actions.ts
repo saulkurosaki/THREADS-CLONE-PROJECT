fab5b7d6c8c721e139065d78f15e0cdda7b4774e
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-19 04:38:24 -0600

    feat(community.actions.ts): implement removeUserFromCommunity server action

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -200,3 +200,44 @@ export async function addMemberToCommunity(
     throw error;
   }
 }
+
+export async function removeUserFromCommunity(
+  userId: string,
+  communityId: string
+) {
+  try {
+    connectToDB();
+
+    const userIdObject = await User.findOne({ id: userId }, { _id: 1 });
+    const communityIdObject = await Community.findOne(
+      { id: communityId },
+      { _id: 1 }
+    );
+
+    if (!userIdObject) {
+      throw new Error("User not found");
+    }
+
+    if (!communityIdObject) {
+      throw new Error("Community not found");
+    }
+
+    // Remove the user's _id from the members array in the community
+    await Community.updateOne(
+      { _id: communityIdObject._id },
+      { $pull: { members: userIdObject._id } }
+    );
+
+    // Remove the community's _id from the communities array in the user
+    await User.updateOne(
+      { _id: userIdObject._id },
+      { $pull: { communities: communityIdObject._id } }
+    );
+
+    return { success: true };
+  } catch (error) {
+    // Handle any errors
+    console.error("Error removing user from community:", error);
+    throw error;
+  }
+}
