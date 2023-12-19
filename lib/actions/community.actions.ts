e318604221758fac074c264db1c061f2f116ebb9
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-19 04:35:05 -0600

    feat(community.actions.ts): implement fetchCommunityPosts server actions

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -69,3 +69,36 @@ export async function fetchCommunityDetails(id: string) {
     throw error;
   }
 }
+
+export async function fetchCommunityPosts(id: string) {
+  try {
+    connectToDB();
+
+    const communityPosts = await Community.findById(id).populate({
+      path: "threads",
+      model: Thread,
+      populate: [
+        {
+          path: "author",
+          model: User,
+          select: "name image id", // Select the "name" and "_id" fields from the "User" model
+        },
+        {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "image _id", // Select the "name" and "_id" fields from the "User" model
+          },
+        },
+      ],
+    });
+
+    return communityPosts;
+  } catch (error) {
+    // Handle any errors
+    console.error("Error fetching community posts:", error);
+    throw error;
+  }
+}
