748d2e5002a0e31e615365abb421f3b3a45c602a
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-19 04:39:15 -0600

    feat(community.actions.ts): implement updateCommunityInfo server action

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -241,3 +241,30 @@ export async function removeUserFromCommunity(
     throw error;
   }
 }
+
+export async function updateCommunityInfo(
+  communityId: string,
+  name: string,
+  username: string,
+  image: string
+) {
+  try {
+    connectToDB();
+
+    // Find the community by its _id and update the information
+    const updatedCommunity = await Community.findOneAndUpdate(
+      { id: communityId },
+      { name, username, image }
+    );
+
+    if (!updatedCommunity) {
+      throw new Error("Community not found");
+    }
+
+    return updatedCommunity;
+  } catch (error) {
+    // Handle any errors
+    console.error("Error updating community information:", error);
+    throw error;
+  }
+}
