a9e1ec6e19fabfcb1d04092d39afd83be3077dc4
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-19 04:36:25 -0600

    feat(community.actions.ts): implement fetchCommunities server action

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -102,3 +102,59 @@ export async function fetchCommunityPosts(id: string) {
     throw error;
   }
 }
+
+export async function fetchCommunities({
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
+
+    // Calculate the number of communities to skip based on the page number and page size.
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Create a case-insensitive regular expression for the provided search string.
+    const regex = new RegExp(searchString, "i");
+
+    // Create an initial query object to filter communities.
+    const query: FilterQuery<typeof Community> = {};
+
+    // If the search string is not empty, add the $or operator to match either username or name fields.
+    if (searchString.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    // Define the sort options for the fetched communities based on createdAt field and provided sort order.
+    const sortOptions = { createdAt: sortBy };
+
+    // Create a query to fetch the communities based on the search and sort criteria.
+    const communitiesQuery = Community.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate("members");
+
+    // Count the total number of communities that match the search criteria (without pagination).
+    const totalCommunitiesCount = await Community.countDocuments(query);
+
+    const communities = await communitiesQuery.exec();
+
+    // Check if there are more communities beyond the current page.
+    const isNext = totalCommunitiesCount > skipAmount + communities.length;
+
+    return { communities, isNext };
+  } catch (error) {
+    console.error("Error fetching communities:", error);
+    throw error;
+  }
+}
