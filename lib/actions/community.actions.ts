1524e9a1a7b7467a76516baba77548f23e979c77
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-19 04:37:27 -0600

    feat(community.actions.ts): implement addMemberToCommunity server action

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -158,3 +158,45 @@ export async function fetchCommunities({
     throw error;
   }
 }
+
+export async function addMemberToCommunity(
+  communityId: string,
+  memberId: string
+) {
+  try {
+    connectToDB();
+
+    // Find the community by its unique id
+    const community = await Community.findOne({ id: communityId });
+
+    if (!community) {
+      throw new Error("Community not found");
+    }
+
+    // Find the user by their unique id
+    const user = await User.findOne({ id: memberId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Check if the user is already a member of the community
+    if (community.members.includes(user._id)) {
+      throw new Error("User is already a member of the community");
+    }
+
+    // Add the user's _id to the members array in the community
+    community.members.push(user._id);
+    await community.save();
+
+    // Add the community's _id to the communities array in the user
+    user.communities.push(community._id);
+    await user.save();
+
+    return community;
+  } catch (error) {
+    // Handle any errors
+    console.error("Error adding member to community:", error);
+    throw error;
+  }
+}
