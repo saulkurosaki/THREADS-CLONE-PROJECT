966318b231f7210375814e0bf909efde56db607b
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-19 04:40:05 -0600

    feat(community.action.ts): implement deleteCommunity server action

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -268,3 +268,37 @@ export async function updateCommunityInfo(
     throw error;
   }
 }
+
+export async function deleteCommunity(communityId: string) {
+  try {
+    connectToDB();
+
+    // Find the community by its ID and delete it
+    const deletedCommunity = await Community.findOneAndDelete({
+      id: communityId,
+    });
+
+    if (!deletedCommunity) {
+      throw new Error("Community not found");
+    }
+
+    // Delete all threads associated with the community
+    await Thread.deleteMany({ community: communityId });
+
+    // Find all users who are part of the community
+    const communityUsers = await User.find({ communities: communityId });
+
+    // Remove the community from the 'communities' array for each user
+    const updateUserPromises = communityUsers.map((user) => {
+      user.communities.pull(communityId);
+      return user.save();
+    });
+
+    await Promise.all(updateUserPromises);
+
+    return deletedCommunity;
+  } catch (error) {
+    console.error("Error deleting community: ", error);
+    throw error;
+  }
+}
