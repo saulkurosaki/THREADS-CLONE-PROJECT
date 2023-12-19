d43c219c53dae77de3182ca6426e94a5c1feaabd
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-19 04:32:14 -0600

    feat(community.actions.ts): implement createCommunity server action

---
+++ lib/actions/community.actions.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import { FilterQuery, SortOrder } from "mongoose";
+
+import Community from "../models/community.model";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+
+import { connectToDB } from "../mongoose";
+
+export async function createCommunity(
+  id: string,
+  name: string,
+  username: string,
+  image: string,
+  bio: string,
+  createdById: string // Change the parameter name to reflect it's an id
+) {
+  try {
+    connectToDB();
+
+    // Find the user with the provided unique id
+    const user = await User.findOne({ id: createdById });
+
+    if (!user) {
+      throw new Error("User not found"); // Handle the case if the user with the id is not found
+    }
+
+    const newCommunity = new Community({
+      id,
+      name,
+      username,
+      image,
+      bio,
+      createdBy: user._id, // Use the mongoose ID of the user
+    });
+
+    const createdCommunity = await newCommunity.save();
+
+    // Update User model
+    user.communities.push(createdCommunity._id);
+    await user.save();
+
+    return createdCommunity;
+  } catch (error) {
+    // Handle any errors
+    console.error("Error creating community:", error);
+    throw error;
+  }
+}
