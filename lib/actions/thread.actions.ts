858566a34677787d50bca72a409cd3467ed77238
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-20 14:30:25 -0600

    feat(thread.actions.ts): update addCommentToThread server action for communities feature

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -134,40 +134,41 @@ export async function fetchThreadById(threadId: string) {
   }
 }
 
-export const addCommentToThread = async (
+export async function addCommentToThread(
   threadId: string,
   commentText: string,
   userId: string,
   path: string
-) => {
+) {
   connectToDB();
 
   try {
-    // Find the original Thread by its Id
+    // Find the original thread by its ID
     const originalThread = await Thread.findById(threadId);
 
     if (!originalThread) {
-      throw new Error("Thread not found.");
+      throw new Error("Thread not found");
     }
 
-    // Create a new thread with the comment text
+    // Create the new comment thread
     const commentThread = new Thread({
       text: commentText,
       author: userId,
-      parentId: threadId,
+      parentId: threadId, // Set the parentId to the original thread's ID
     });
 
-    // Save the new thread
+    // Save the comment thread to the database
     const savedCommentThread = await commentThread.save();
 
-    // Update the original thread to include the new comment
+    // Add the comment thread's ID to the original thread's children array
     originalThread.children.push(savedCommentThread._id);
 
-    // Save the original thread
+    // Save the updated original thread to the database
     await originalThread.save();
 
     revalidatePath(path);
-  } catch (error: any) {
-    throw new Error(`Error adding comment to thread: ${error.message}`);
+  } catch (err) {
+    console.error("Error while adding comment:", err);
+    throw new Error("Unable to add comment");
   }
-};
+}
