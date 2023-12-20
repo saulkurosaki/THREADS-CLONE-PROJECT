34fd89e7f9e76cfe70ea756c63d5f82644f3177a
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-20 04:07:58 -0600

    feat(thread.actions.ts): update fetchPosts server action for communities feature

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -51,41 +51,45 @@ export async function createThread({
   }
 }
 
-export const fetchPosts = async (pageNumber = 1, pageSize = 20) => {
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
   connectToDB();
 
-  try {
-    // Calculate the number of posts to skip
-    const skipAmount = (pageNumber - 1) * pageSize;
+  // Calculate the number of posts to skip based on the page number and page size.
+  const skipAmount = (pageNumber - 1) * pageSize;
 
-    // Fetch the posts that have no parents (top-level threads...)
-    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
-      .sort({ createdAt: "desc" })
-      .skip(skipAmount)
-      .limit(pageSize)
-      .populate({ path: "author", model: User })
-      .populate({
-        path: "children",
-        populate: {
-          path: "author",
-          model: User,
-          select: "_id name parentId image",
-        },
-      });
-
-    const totalPostsCount = await Thread.countDocuments({
-      parentId: { $in: [null, undefined] },
+  // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: "author",
+      model: User,
+    })
+    .populate({
+      path: "community",
+      model: Community,
+    })
+    .populate({
+      path: "children", // Populate the children field
+      populate: {
+        path: "author", // Populate the author field within children
+        model: User,
+        select: "_id name parentId image", // Select only _id and username fields of the author
+      },
     });
 
-    const posts = await postsQuery.exec();
+  // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  }); // Get the total count of posts
 
-    const isNext = totalPostsCount > skipAmount + posts.length;
+  const posts = await postsQuery.exec();
 
-    return { posts, isNext };
-  } catch (error: any) {
-    throw new Error(`Error getting the posts: ${error.message}`);
-  }
-};
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
 
 export const fetchThreadById = async (id: string) => {
   connectToDB();
