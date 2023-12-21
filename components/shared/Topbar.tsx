283d25831e935db9de86f8f35feb4edcfc870644
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-21 15:03:38 -0600

    fix(Topbar): bug fixing

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,13 +1,13 @@
-import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 
-const Topbar = () => {
+function Topbar() {
   return (
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
-        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <Image src="/logo.svg" alt="logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
 
@@ -38,6 +38,6 @@ const Topbar = () => {
       </div>
     </nav>
   );
-};
+}
 
 export default Topbar;
