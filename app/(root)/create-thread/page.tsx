22b5d5443066d0790fd9d2a4a7f467e9bfc26fa9
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-21 14:30:42 -0600

    fix(create thread page): bug fixing

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,15 +1,15 @@
-import PostThread from "@/components/forms/PostThread";
-import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-const CreateThreadsPage = async () => {
-  const user = await currentUser();
+import PostThread from "@/components/forms/PostThread";
+import { fetchUser } from "@/lib/actions/user.actions";
 
+async function Page() {
+  const user = await currentUser();
   if (!user) return null;
 
+  // fetch organization list created by user
   const userInfo = await fetchUser(user.id);
-
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
@@ -19,6 +19,6 @@ const CreateThreadsPage = async () => {
       <PostThread userId={userInfo._id} />
     </>
   );
-};
+}
 
-export default CreateThreadsPage;
+export default Page;
