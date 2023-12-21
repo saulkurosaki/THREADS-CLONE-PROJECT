7093f3f0ad8f95132ac433414b389f10c8e2f55a
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-21 14:48:36 -0600

    fix(UserCard): bug fixing

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
+
+import { Button } from "../ui/button";
 
 interface Props {
   id: string;
@@ -12,19 +13,22 @@ interface Props {
   personType: string;
 }
 
-const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
+function UserCard({ id, name, username, imgUrl, personType }: Props) {
   const router = useRouter();
+
+  const isCommunity = personType === "Community";
 
   return (
     <article className="user-card">
       <div className="user-card_avatar">
-        <Image
-          src={imgUrl}
-          alt="logo"
-          width={48}
-          height={48}
-          className="rounded-full"
-        />
+        <div className="relative h-12 w-12">
+          <Image
+            src={imgUrl}
+            alt="user_logo"
+            fill
+            className="rounded-full object-cover"
+          />
+        </div>
 
         <div className="flex-1 text-ellipsis">
           <h4 className="text-base-semibold text-light-1">{name}</h4>
@@ -34,12 +38,18 @@ const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
 
       <Button
         className="user-card_btn"
-        onClick={() => router.push(`/profile/${id}`)}
+        onClick={() => {
+          if (isCommunity) {
+            router.push(`/communities/${id}`);
+          } else {
+            router.push(`/profile/${id}`);
+          }
+        }}
       >
         View
       </Button>
     </article>
   );
-};
+}
 
 export default UserCard;
