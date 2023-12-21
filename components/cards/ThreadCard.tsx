e23e1da7fe6a72cd843af1ae4aa410c9c314291d
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-21 14:19:00 -0600

    feat(ThreadCard): implement thread card community info display

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -113,6 +114,27 @@ const ThreadCard = ({
             </div>
           </div>
         </div>
+        {/* TODO: Delete Thread */}
+        {/* TODO: Show comment logos */}
+
+        {!comments && community && (
+          <Link
+            href={`/communities/${community.id}`}
+            className="mt-5 flex items-center"
+          >
+            <p className="text-subtle-medium text-gray-1">
+              {formatDateString(createdAt)}- {community.name} Community
+            </p>
+
+            <Image
+              src={community.image}
+              alt={community.name}
+              width={14}
+              height={14}
+              className="ml-1 rounded-full object-cover"
+            />
+          </Link>
+        )}
       </div>
     </article>
   );
