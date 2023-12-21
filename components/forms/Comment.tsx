969576e9ee466f54ea9502696ec3f10d58d0ac98
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-21 14:51:36 -0600

    fix(Comment): bug fixing

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,37 +1,35 @@
 "use client";
 
-import * as z from "zod";
+import { z } from "zod";
+import Image from "next/image";
 import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
 } from "@/components/ui/form";
+
 import { Input } from "../ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "../ui/button";
 
-import { usePathname, useRouter } from "next/navigation";
-
-// import { updateUser } from "@/lib/actions/user.actions";
 import { CommentValidation } from "@/lib/validations/thread";
-import Image from "next/image";
 import { addCommentToThread } from "@/lib/actions/thread.actions";
-// import { createThread } from "@/lib/actions/thread.actions";
+
 interface Props {
   threadId: string;
-  currentUserImage: string;
+  currentUserImg: string;
   currentUserId: string;
 }
 
-const Comment = ({ threadId, currentUserImage, currentUserId }: Props) => {
-  const router = useRouter();
+function Comment({ threadId, currentUserImg, currentUserId }: Props) {
   const pathname = usePathname();
 
-  const form = useForm({
+  const form = useForm<z.infer<typeof CommentValidation>>({
     resolver: zodResolver(CommentValidation),
     defaultValues: {
       thread: "",
@@ -51,27 +49,27 @@ const Comment = ({ threadId, currentUserImage, currentUserId }: Props) => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="comment-form">
+      <form className="comment-form" onSubmit={form.handleSubmit(onSubmit)}>
         <FormField
           control={form.control}
           name="thread"
           render={({ field }) => (
-            <FormItem className="flex gap-3 w-full items-center">
+            <FormItem className="flex w-full items-center gap-3">
               <FormLabel>
                 <Image
-                  src={currentUserImage}
-                  alt="Profile Image"
+                  src={currentUserImg}
+                  alt="current_user"
                   width={48}
                   height={48}
                   className="rounded-full object-cover"
                 />
               </FormLabel>
-              <FormControl className="border-none bg-transparent ">
+              <FormControl className="border-none bg-transparent">
                 <Input
                   type="text"
+                  {...field}
                   placeholder="Comment..."
                   className="no-focus text-light-1 outline-none"
-                  {...field}
                 />
               </FormControl>
             </FormItem>
@@ -84,6 +82,6 @@ const Comment = ({ threadId, currentUserImage, currentUserId }: Props) => {
       </form>
     </Form>
   );
-};
+}
 
 export default Comment;
