194795cd249e85d0776f485d9b401f0d1867c50a
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-21 14:43:25 -0600

    add clerck appearence base theme dark to the root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,20 @@
-import "../globals.css";
+import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-import Topbar from "@/components/shared/Topbar";
+import "../globals.css";
 import Leftsidebar from "@/components/shared/Leftsidebar";
-import Rightsidebar from "@/components/shared/Rightsidebar";
 import Bottombar from "@/components/shared/Bottombar";
+import Rightsidebar from "@/components/shared/Rightsidebar";
+import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Threads",
-  description: "A Next.js 13 Meta Threads App.",
+  description: "A Next.js 13 Meta Threads application",
 };
 
 export default function RootLayout({
@@ -21,18 +23,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
         <body className={inter.className}>
           <Topbar />
 
           <main className="flex flex-row">
             <Leftsidebar />
-
             <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
             </section>
-
+            {/* @ts-ignore */}
             <Rightsidebar />
           </main>
 
