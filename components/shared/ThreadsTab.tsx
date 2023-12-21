85a72ae3b50de48e94f877d543cefb60dffe0e61
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-21 15:02:55 -0600

    feat(ThreadsTab): implement threads tab community feature

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,7 +1,36 @@
-import { fetchUserPosts } from "@/lib/actions/user.actions";
-import React from "react";
-import ThreadCard from "../cards/ThreadCard";
 import { redirect } from "next/navigation";
+
+import { fetchCommunityPosts } from "@/lib/actions/community.actions";
+import { fetchUserPosts } from "@/lib/actions/user.actions";
+
+import ThreadCard from "../cards/ThreadCard";
+
+interface Result {
+  name: string;
+  image: string;
+  id: string;
+  threads: {
+    _id: string;
+    text: string;
+    parentId: string | null;
+    author: {
+      name: string;
+      image: string;
+      id: string;
+    };
+    community: {
+      id: string;
+      name: string;
+      image: string;
+    } | null;
+    createdAt: string;
+    children: {
+      author: {
+        image: string;
+      };
+    }[];
+  }[];
+}
 
 interface Props {
   currentUserId: string;
@@ -9,18 +38,26 @@ interface Props {
   accountType: string;
 }
 
-const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
-  let result = await fetchUserPosts(accountId);
+async function ThreadsTab({ currentUserId, accountId, accountType }: Props) {
+  let result: Result;
 
-  if (!result) redirect("/");
+  if (accountType === "Community") {
+    result = await fetchCommunityPosts(accountId);
+  } else {
+    result = await fetchUserPosts(accountId);
+  }
+
+  if (!result) {
+    redirect("/");
+  }
 
   return (
     <section className="mt-9 flex flex-col gap-10">
-      {result.threads.map((thread: any) => (
+      {result.threads.map((thread) => (
         <ThreadCard
           key={thread._id}
           id={thread._id}
-          currentUser={currentUserId}
+          currentUserId={currentUserId}
           parentId={thread.parentId}
           content={thread.text}
           author={
@@ -32,14 +69,17 @@ const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
                   id: thread.author.id,
                 }
           }
-          community={thread.community} //TODO
+          community={
+            accountType === "Community"
+              ? { name: result.name, id: result.id, image: result.image }
+              : thread.community
+          }
           createdAt={thread.createdAt}
           comments={thread.children}
-          isComment
         />
       ))}
     </section>
   );
-};
+}
 
 export default ThreadsTab;
