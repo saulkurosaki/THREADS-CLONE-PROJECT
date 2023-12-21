e865883c81933849a70a65d697e12d6c205040fa
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-21 14:32:16 -0600

    fix(profile/id page): bug fixing

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,13 +1,16 @@
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import ThreadsTab from "@/components/shared/ThreadsTab";
-import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
-import { profileTabs } from "@/constants";
-import { fetchUser } from "@/lib/actions/user.actions";
-import { currentUser } from "@clerk/nextjs";
 import Image from "next/image";
+import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-const Profile = async ({ params }: { params: { id: string } }) => {
+import { profileTabs } from "@/constants";
+
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
   if (!user) return null;
 
@@ -15,7 +18,7 @@ const Profile = async ({ params }: { params: { id: string } }) => {
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
-    <div>
+    <section>
       <ProfileHeader
         accountId={userInfo.id}
         authUserId={user.id}
@@ -41,7 +44,7 @@ const Profile = async ({ params }: { params: { id: string } }) => {
 
                 {tab.label === "Threads" && (
                   <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
-                    {userInfo?.threads?.length}
+                    {userInfo.threads.length}
                   </p>
                 )}
               </TabsTrigger>
@@ -53,6 +56,7 @@ const Profile = async ({ params }: { params: { id: string } }) => {
               value={tab.value}
               className="w-full text-light-1"
             >
+              {/* @ts-ignore */}
               <ThreadsTab
                 currentUserId={user.id}
                 accountId={userInfo.id}
@@ -62,8 +66,7 @@ const Profile = async ({ params }: { params: { id: string } }) => {
           ))}
         </Tabs>
       </div>
-    </div>
+    </section>
   );
-};
-
-export default Profile;
+}
+export default Page;
