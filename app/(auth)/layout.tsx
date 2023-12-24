91c7fc3b0fb983fc19e732678c6e75cb0308c125
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-23 23:54:26 -0600

    style(auth layout): authentication box style bug fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -25,7 +25,11 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   );
